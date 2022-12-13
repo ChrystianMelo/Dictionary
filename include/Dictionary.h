7b0a0819ca12723496351bc54e666d17ae3ab944
chrystianmelo <chrysmelao@gmail.com>
2022-12-13 16:40:41 -0300

    Fixed the metods documentation. Fixed Dictionary methods args.

--- include/Dictionary.h
+++ include/Dictionary.h
@@ -1,19 +1,62 @@
+/**
+ * @file Dictionary.h
+ * @author Chrystian Melo (meloo.chrys@gmail.com)
+ * @brief Classe dicionário. Esta dicionário deve
+ * permitir consultas, inserções, retiradas, impressão
+ * ordenada e atualização de uma entrada.
+ * @version 0.1
+ * @date 2022-12-13
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+/**
+ * @brief Definição da classe verbete.
+ *
+ */
 class Verbete
 {
 };
 
+/**
+ * @brief Definição da classe dicionário.
+ *
+ */
 class Dictionary
 {
 public:
+    /**
+     * @brief Construtor da classe.
+     */
     Dictionary(int n);
-    int pesquisaDic(Dictionary *dic, Verbete *it);
-    int insereDic(Dictionary *dic, Verbete *it);
-    int imprimeDic(Dictionary *dic);
-    int atualizaDic(Dictionary *dic, Verbete *it);
-    int removeDic(Dictionary *dic, Verbete *it);
-    int destroiDic(Dictionary *dic);
+
+    /**
+     * @brief Destrutor da classe.
+     */
+    ~Dictionary();
+
+    /**
+     * @brief Metodo de pesquisa no dicionário.
+     */
+    int pesquisaDic(Verbete *it);
+    /**
+     * @brief Metodo de insere no dicionário.
+     */
+    int insereDic(Verbete *it);
+    /**
+     * @brief Metodo de imprime o dicionário.
+     */
+    int imprimeDic();
+    /**
+     * @brief Metodo de atualiza um verbete presente no dicionário.
+     */
+    int atualizaDic(Verbete *it);
+    /**
+     * @brief Metodo de remove um verbete do dicionário.
+     */
+    int removeDic(Verbete *it);
 };
 #endif
--- src/Dictionary.cc
+++ src/Dictionary.cc
@@ -1,9 +1,9 @@
 #include "Dictionary.h"
 
 Dictionary::Dictionary(int n) {}
-int Dictionary::pesquisaDic(Dictionary *dic, Verbete *it) { return 0; }
-int Dictionary::insereDic(Dictionary *dic, Verbete *it) { return 0; }
-int Dictionary::imprimeDic(Dictionary *dic) { return 0; }
-int Dictionary::atualizaDic(Dictionary *dic, Verbete *it) { return 0; }
-int Dictionary::removeDic(Dictionary *dic, Verbete *it) { return 0; }
-int Dictionary::destroiDic(Dictionary *dic) { return 0; }
+Dictionary::~Dictionary() {}
+int Dictionary::pesquisaDic(Verbete *it) { return 0; }
+int Dictionary::insereDic(Verbete *it) { return 0; }
+int Dictionary::imprimeDic() { return 0; }
+int Dictionary::atualizaDic(Verbete *it) { return 0; }
+int Dictionary::removeDic(Verbete *it) { return 0; }
